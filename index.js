d8b8ee137a247af1b6c9fa2acdfdf9e0eb324f92
NewbieCoderH <legendaryh1381@gmail.com>
2023-04-11 15:22:33 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let generatePasswordBtn = document.getElementById("generate-password-btn")
+let firstInputEl = document.getElementById("first-input-el")
+let secondInputEl = document.getElementById("second-input-el")
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+generatePasswordBtn.addEventListener("click",function(){
+    firstInputEl.value = ""
+    secondInputEl.value = ""
+    for(let i = 0 ; i < 15 ; i++ ){
+        let randomIndexOne = Math.floor(Math.random() * characters.length)
+        let randomIndexTwo = Math.floor(Math.random() * characters.length)
+        firstInputEl.value += characters[randomIndexOne]
+        secondInputEl.value += characters[randomIndexTwo]
+    }
+
+})
